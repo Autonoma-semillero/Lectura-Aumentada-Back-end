497cfda0bfd35c9c62795abd04e868532315546f
Juan Yonda <juancamilo@tscagency.org>
2026-09-08 15:37:24 -0500

    feat(assets): expose typed WebAR marker lookup

--- src/modules/assets/presentation/assets.controller.ts
+++ src/modules/assets/presentation/assets.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 import { AssetsService } from '../application/assets.service';
+import { AssetResponseDto } from '../dto/asset-response.dto';
 import { CreateAssetDto } from '../dto/create-asset.dto';
 
 @ApiTags('assets')
@@ -14,7 +15,10 @@ export class AssetsController {
   }
 
   @Get('marker/:markerId')
-  async findByMarker(@Param('markerId') markerId: string): Promise<unknown> {
+  @ApiParam({ name: 'markerId', description: 'Identificador estable detectado por el motor AR' })
+  @ApiOkResponse({ type: AssetResponseDto })
+  @ApiNotFoundResponse({ description: 'El marcador no tiene una unidad de aprendizaje asociada' })
+  async findByMarker(@Param('markerId') markerId: string): Promise<AssetResponseDto> {
     return this.assetsService.findByMarker(markerId);
   }
 
